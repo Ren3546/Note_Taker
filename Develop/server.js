23c2f410baaa8fadc8a18b95ceacd46d331f16b8
Rengo _ <renatovaldez6879@gmail.com>
2024-01-26 22:22:45 -0500

    setup boiler plate for server.js

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const PORT = process.env.port || 3001;
+const path = require('path');
+const app = express();
+
+//parsing JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
