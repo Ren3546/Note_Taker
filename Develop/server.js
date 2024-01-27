bd0aa080f5d77662db40a88aeff2cc7f311b57a2
Rengo _ <renatovaldez6879@gmail.com>
2024-01-26 23:17:22 -0500

    post request to make new notes and render to the page

--- Develop/server.js
+++ Develop/server.js
@@ -2,23 +2,52 @@ const express = require('express');
 const PORT = process.env.port || 3001;
 const path = require('path');
 const app = express();
+const noteList = require('./db/db.json')
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
 //parsing JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
-// GET Route for homepage
-app.get('*', (req, res) =>
+// GET Route 
+app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
+// GET Route 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+//Get all notes
+app.get('/api/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+//post
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+
+    const { title, text } = req.body
+
+      const newNote = {
+        title,
+        text,
+      };
+
+      readAndAppend(newNote, './db/db.json');
+
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    });
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
